24aeb109fb64a1986af7f20892f438017ecf69d7
William Chang <wilicw1346@gmail.com>
2023-07-20 13:36:22 +0800

    Modify install script

--- custom/chadrc.lua
+++ custom/chadrc.lua
@@ -5,7 +5,7 @@ local M = {}
 local highlights = require "custom.highlights"
 
 M.ui = {
-  theme = "ayu_dark",
+  theme = "jellybeans",
 
   hl_override = highlights.override,
   hl_add = highlights.add,
--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
-git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1 && nvim
-ln -s ./custom ~/.config/nvim/lua/custom
+#!/usr/bin/env bash
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
+rm -rf ~/.config/nvim/lua/custom
+ln -s $(pwd)/custom ~/.config/nvim/lua/custom
+nvim
 
