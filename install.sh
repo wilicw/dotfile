3ccd8a8d6cd8c7c5c0eda443f466b8178e984fae
William Chang <wilicw1346@gmail.com>
2023-09-12 00:05:37 +0800

    Add tmux config

--- install.sh
+++ install.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
+
+# Tmux config
+mkdir -p ~/.config/tmux
+mkdir -p ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $(pwd)/tmux.conf ~/.config/tmux/tmux.conf
+
 git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 rm -rf ~/.config/nvim/lua/custom
 ln -s $(pwd)/custom ~/.config/nvim/lua/custom
