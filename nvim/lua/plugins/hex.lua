437b76b9446720e33b98c93273d66a3311d1cd67
William Chang <wilicw1346@gmail.com>
2025-10-11 01:17:46 +0800

    Update lazy vim

---
+++ nvim/lua/plugins/hex.lua
@@ -0,0 +1,3 @@
+return {
+  { 'RaafatTurki/hex.nvim' }
+}
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,40 +1,41 @@
-local cmp = require("cmp")
-
 return {
   {
-   "L3MON4D3/LuaSnip",
-   -- follow latest release.
-   version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-   -- install jsregexp (optional!).
-   build = "make install_jsregexp"
+    "L3MON4D3/LuaSnip",
+    -- follow latest release.
+    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+    -- install jsregexp (optional!).
+    build = "make install_jsregexp",
   },
   {
     "hrsh7th/nvim-cmp",
-    opts = {
-      mapping = cmp.mapping.preset.insert({
-        ["<Up>"] = cmp.mapping.select_prev_item(),
-        ["<Down>"] = cmp.mapping.select_next_item(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-j>"] = cmp.mapping(function(fallback)
-          if require("luasnip").jumpable(1) then
-            require("luasnip").jump(1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<C-k>"] = cmp.mapping(function(fallback)
-          if require("luasnip").jumpable(-1) then
-            require("luasnip").jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      }),
-    },
+    opts = function()
+      local cmp = require("cmp")
+      return {
+        mapping = cmp.mapping.preset.insert({
+          ["<Up>"] = cmp.mapping.select_prev_item(),
+          ["<Down>"] = cmp.mapping.select_next_item(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+          ["<C-j>"] = cmp.mapping(function(fallback)
+            if require("luasnip").jumpable(1) then
+              require("luasnip").jump(1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+          ["<C-k>"] = cmp.mapping(function(fallback)
+            if require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        }),
+      }
+    end,
   },
   {
-    'stevearc/conform.nvim',
+    "stevearc/conform.nvim",
     opts = {},
   },
   {
